6a86d572ba711042b8a81d55b260b7759a0a72f7
zero0r1 <leahdizonth@gmail.com>
2017-09-13 17:57:34 +0800

    Commit

---
+++ ContosoUniversity/ContosoUniversity/DAL/SchoolContext.cs
@@ -0,0 +1,31 @@
+﻿using ContosoUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        const string _nameOrConnectionString = "SchoolContext";
+
+        public SchoolContext() : base(_nameOrConnectionString)
+        {
+        }
+
+        //The pluralized forms of entity class names are used as table names.
+        //<code>
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        //</code>
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,21 @@
+﻿namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; }
+
+        //A Course entity can be related to any number of Enrollment entities.
+        public virtual Course Course { get; set; }
+
+        //A Student entity can be related to any number of Enrollment entities.
+        public virtual Student Student { get; set; }
+    }
+}
